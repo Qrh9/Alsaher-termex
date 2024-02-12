40edd8df74d439a6da7acdce339ac413180183eb
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-02-12 23:27:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import env
-import time
 import logging
 from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
-logging.basicenv(
+logging.basicConfig(
     level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
+)  # type: ignore
 
-StartTime = time.time()
 app = Client(
-    "Anonymous",
+    ":memory:",
     api_id=env.API_ID,
     api_hash=env.API_HASH,
     bot_token=env.BOT_TOKEN,
-    in_memory=True,
-    plugins=dict(root="StringGenBot"),
+    plugins=dict(root="StringSessionBot"),
 )
 
 
 if __name__ == "__main__":
-    print("Starting the String Generator Bot...")
+    print("Starting the bot")
     try:
         app.start()
     except (ApiIdInvalid, ApiIdPublishedFlood):
@@ -29,7 +26,7 @@ if __name__ == "__main__":
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
     uname = app.get_me().username
-    print(f"@{uname} started successfully !")
+    print(f"@{uname} is now running!")
     idle()
     app.stop()
-    print("Bot stopped. Bye !")
+    print("Bot stopped. Alvida!")
