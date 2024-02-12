c8647d57c5b110e52645d0dcb9db51a4ab6c98d1
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-02-12 20:42:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = os.getenv("API_ID", "").strip()
+API_HASH = os.getenv("API_HASH", "").strip()#
+BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/SXYO3")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = 5835316914
+if not API_ID:
+    print("No API_ID found. Exiting...")
+    raise SystemExit
+if not API_HASH:
+    print("No API_HASH found. Exiting...")
+    raise SystemExit
+if not BOT_TOKEN:
+    print("No BOT_TOKEN found. Exiting...")
+    raise SystemExit
+if not DATABASE_URL:
+    print("No DATABASE_URL found. Exiting...")
+    raise SystemExit
 
-DATABASE_URL = os.getenv("DATABASE_URL","").strip()
-MUST_JOIN = getenv("MUST_JOIN", None)
+try:
+    API_ID = int(API_ID)
+except ValueError:
+    print("API_ID is not a valid integer. Exiting...")
+    raise SystemExit
+
+if "postgres" in DATABASE_URL and "postgresql" not in DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
