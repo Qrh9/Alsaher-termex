b9c1972d0764890136aa459d5a8665ab85d20f7e
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-02-12 20:40:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = 5835316914
 
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
+DATABASE_URL = os.getenv("DATABASE_URL","").strip()
 MUST_JOIN = getenv("MUST_JOIN", None)
