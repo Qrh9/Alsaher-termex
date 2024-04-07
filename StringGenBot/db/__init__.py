21a8084d250e9fb3cacd58a0200b760aa5d872a0
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-04-07 08:09:11 +0300

    Update __init__.py

--- StringGenBot/db/__init__.py
+++ StringGenBot/db/__init__.py
@@ -1,8 +1,13 @@
 from env import DATABASE_URL
-from . import db
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+BASE = declarative_base()
+
 
 
 def start() -> scoped_session:
