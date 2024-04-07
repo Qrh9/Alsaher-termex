e80876ba37e29d9ae0f7b1285b3dfa71f9bcbf64
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-04-07 08:04:58 +0300

    Update __init__.py

--- StringGenBot/db/__init__.py
+++ StringGenBot/db/__init__.py
@@ -1,5 +1,5 @@
 from env import DATABASE_URL
-
+from . import db
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
