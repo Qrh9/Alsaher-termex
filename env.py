3232a0735e58b10527e8c498682630e2c89fec66
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-04-07 07:17:33 +0300

    Update env.py

--- env.py
+++ env.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 OWNER_ID= 5835316914
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()#
+API_ID = 29594794
+API_HASH = "d4b492451c79aad8eee7592ebc2220d4"
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "postgres://hbbudrdw:WQ3Yef0yJF3T-jUK8uDxBRH7ls1e-yxM@jelani.db.elephantsql.com/hbbudrdw" )
-MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/SXYO3")
+DATABASE_URL ="postgres://hbbudrdw:WQ3Yef0yJF3T-jUK8uDxBRH7ls1e-yxM@jelani.db.elephantsql.com/hbbudrdw"
+MUST_JOIN = "https://t.me/SXYO3"
 
 if not API_ID:
     print("No API_ID found. Exiting...")
